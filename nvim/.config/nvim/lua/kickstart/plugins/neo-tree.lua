5c955f57fcddc86090f77d3b116bb24bd4c29e55
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2025-02-05 14:38:04 +0200

    Updated nvim neo-tree conf

--- nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { 'e', ':Neotree reveal right<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['e'] = 'close_window',
         },
       },
     },
